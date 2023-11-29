fbdc68788b735d0155483cac949457391b5899b6
bhuvaneswaran <bwaran001@gamil.com>
2023-11-30 02:18:33 +0530

    new

--- app/run1.sh
+++ app/run1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-apt-get update -y
-apt-get install pip python3-venv  mysql-server git vim -y
-git clone https://github.com/BhuvanesWaran00/Sample_e-Commerce.git
-mv Sample_e-Commerce app
-cd app
 python3 -m venv venv
 source venv/bin/activate
 pip install -r req.txt
