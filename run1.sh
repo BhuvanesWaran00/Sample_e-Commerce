1fad7de541abb0297133c444d641989fd3a659b6
bhuvaneswaran <bwaran001@gamil.com>
2023-11-29 23:19:35 +0530

    docker file update

--- run1.sh
+++ run1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install pip python3-venv  mysql-server git -y
+apt-get update -y
+apt-get install pip python3-venv  mysql-server git vim -y
 git clone https://github.com/BhuvanesWaran00/Sample_e-Commerce.git
 mv Sample_e-Commerce app
 cd app
 python3 -m venv venv
 source venv/bin/activate
 pip install -r req.txt
-sudo cp service.txt /etc/systemd/system/app.service
-sudo chmod 100 run1.sh
-sudo chmod 100 run2.sh
-sudo chmod 100 run3.sh
-sudo ./run2.sh
-sudo ./run3.sh
-gunicorn -b 0.0.0.0:9900 app:app
+cp service.txt /etc/systemd/system/app.service
+chmod 100 run1.sh
+chmod 100 run2.sh
+chmod 100 run3.sh
+./run2.sh
+./run3.sh
+gunicorn -b 0.0.0.0:9000 app:app
--- run2.sh
+++ run2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo systemctl daemon-reload
-sudo systemctl start app
-sudo systemctl enable app
+systemctl daemon-reload
+systemctl start app
+systemctl enable app
